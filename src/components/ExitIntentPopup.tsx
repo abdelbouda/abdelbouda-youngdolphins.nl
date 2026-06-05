14e283e95899af9d8d3fd870b4984ee5b8bfb9f8
Abdel Bouda <abdelbouda@gmail.com>
2026-06-05 12:27:15 +0200

    Fix: Firebase build error - downgrade naar stabiele versies

--- src/components/ExitIntentPopup.tsx
+++ src/components/ExitIntentPopup.tsx
@@ -11,7 +11,7 @@ export default function ExitIntentPopup({ onClose }: ExitIntentPopupProps) {
   const { t, language } = useLanguage();
 
   useEffect(() => {
-    let timer: NodeJS.Timeout;
+    let timer: ReturnType<typeof setTimeout>;
     
     const handleMouseLeave = (e: MouseEvent) => {
       // Als de muur de bovenkant van het scherm verlaat (exit intent)
